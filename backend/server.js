53526720ec51dbc43dfee901e7f1f5b905664b62
Alia <alivamallick9@gmail.com>
2022-09-22 00:15:25 +0530

    works

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require("express");
 // we can check precommit here
-// const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
+// const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-const app = express()
-app.use(express.json)
+const app = express();
+app.use(express.json);
 app.use(
   express.urlencoded({
     extended: false,
   })
-)
+);
 
-app.use('/api/goals', require('./routes/getRoutes'))
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use("/api/goals", require("./routes/getRoutes"));
+app.listen(port, () => console.log(`Server started on port ${port}`));
